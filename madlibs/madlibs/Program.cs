0fb0e0a36eb3760dbcf1ce1568ebeebd110b8608
LavenderLemonade <soe1@zips.uakron.edu>
2021-09-19 21:18:55 -0400

    Streamlining the Code
    Trying to pare this down to something more manageable.

--- madlibs/madlibs/Program.cs
+++ madlibs/madlibs/Program.cs
@@ -6,21 +6,26 @@ namespace madlibs
     {
         static void Main(string[] args)
         {
+
+            string[] libs = new string[5];
+            string[] desc = { "emotion", "game", "game", "s.o", "emotion" };
+            int i = 0;
+
             Console.WriteLine("Welcome to the MadLib! What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Ah, welcome " + name + "!" + " get ready for the MadLib!");
-            Console.WriteLine("Give me an emotion word.");
-            string emo = Console.ReadLine();
-            Console.WriteLine("Give me a board game name.");
-            string board1 = Console.ReadLine();
-            Console.WriteLine("Give me another board game name.");
-            string board2 = Console.ReadLine();
-            Console.WriteLine("Give me the name of someone close to you.");
-            string ally = Console.ReadLine();
-            Console.WriteLine("Finally, give me another emotion word.");
-            string emo2 = Console.ReadLine();
-            Console.WriteLine("I really really " + emo + " playing board games! From " + board1 + " to " + board2 + ", my " + ally + " and I " + emo2 + " playing board games!");
 
+            for (i = 0; i < 5; i++ )
+            {
+                Console.WriteLine("Give me a " + desc[i] + " word");
+                libs[i] = Console.ReadLine();
+               
+            }
+
+            Console.WriteLine("I really really " + libs[0] + " playing board games! From " + libs[1] + " to " + libs[2] + " my sweetie " + libs[3] + " and I really " + libs[4] + " playing games together!");
+
+
+            
            
            
         }
