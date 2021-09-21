b15a6bb88fa121081a54431ff96b76981ca597f1
LavenderLemonade <soe1@zips.uakron.edu>
2021-09-21 14:30:09 -0400

    oh yeah, this is it
    this is the version that allows me to input a string and pull out any of the underscore and replace them with words the user input

--- madlibs/madlibs/Program.cs
+++ madlibs/madlibs/Program.cs
@@ -7,42 +7,22 @@ namespace madlibs
     {
         static void Main(string[] args)
         {
-            /*
-            string[] libs = new string[5];
+            string[] words = new string[10];
             string[] desc = { "emotion", "game", "game", "s.o", "emotion" };
-            int i;
-
+            int i; 
+            var counter = 0; 
+            var substring = Regex.Escape("___"); 
+            var input = "I really really ___ playing board games! From ___ to ___ my sweetie ___ and I really ___ playing games together!";
             Console.WriteLine("Welcome to the MadLib! What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Ah, welcome " + name + "!" + " get ready for the MadLib!");
-
-            for (i = 0; i < 5; i++ )
+            for (i = 0; i < 5; i++)
             {
                 Console.WriteLine("Give me a " + desc[i] + " word");
-                libs[i] = Console.ReadLine();
-               
+                words[i] = Console.ReadLine();
             }
-
-            Console.WriteLine("I really really " + libs[0] + " playing board games! From " + libs[1] + " to " + libs[2] + " my sweetie " + libs[3] + " and I really " + libs[4] + " playing games together!");
-
-
-            */
-            
-            string[] red = { "one", "two", "three" }; // this is the array w strings inside
-            var input = "hey ___ hey ___ hey ___";    // this is the string with spaces I wanna remove
-            var counter = 0;                          // a counter variable
-            var substring = Regex.Escape("___");      // telling Regex what I wanna pull out          
-            var result = Regex.Replace(input, substring, (m) =>$"{red[counter++]}");
+            var result = Regex.Replace(input, substring, (m) => $"{words[counter++]}");
             Console.WriteLine(result);
-                
-           
-           
-            
-            
-
-            
-
-
         }
 
 
