df74c3ff463cdb6e716fa3722b3ec4731a2501d3
LavenderLemonade <soe1@zips.uakron.edu>
2021-09-22 12:39:59 -0400

    adding in file reading
    now we can read straight from text files!

--- madlibs/madlibs/Program.cs
+++ madlibs/madlibs/Program.cs
@@ -11,8 +11,8 @@ namespace madlibs
             string[] desc = { "emotion", "game", "game", "s.o", "emotion" };
             int i; 
             var counter = 0; 
-            var substring = Regex.Escape("___"); 
-            var input = "I really really ___ playing board games! From ___ to ___ my sweetie ___ and I really ___ playing games together!";
+            var substring = Regex.Escape("___");
+            string text = System.IO.File.ReadAllText(@"C:\Users\samue\Desktop\Sam's Folder\Coding Journey\CSharp\madlibs\madlib.txt");
             Console.WriteLine("Welcome to the MadLib! What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("Ah, welcome " + name + "!" + " get ready for the MadLib!");
@@ -21,7 +21,7 @@ namespace madlibs
                 Console.WriteLine("Give me a " + desc[i] + " word");
                 words[i] = Console.ReadLine();
             }
-            var result = Regex.Replace(input, substring, (m) => $"{words[counter++]}");
+            var result = Regex.Replace(text, substring, (m) => $"{words[counter++]}");
             Console.WriteLine(result);
         }
 
